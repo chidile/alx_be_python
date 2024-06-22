48e8c3e04950e480f79b10c7c0b9bf8e1b51ed07
sirchimex <chimexchidile@gmail.com>
2024-06-22 14:00:36 +0100

    Edit basic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -6,8 +6,8 @@ add = number1 + number2
 diff = number2 - number1
 mult = number1 * number2
 
-print("Addition of "+number1+ " and "+ number2+ " is "+add)
-print("Subtraction of "+number2+ " and "+ number1 + " is "+ diff)
-print("Multiplication of "+number1+ " and "+ number2+ " is "+ mult)
+print("Addition of ",number1," and ",number2," is ",add)
+print("Subtraction of ",number2, " and ",number1," is ", diff)
+print("Multiplication of ",number1," and ",number2," is ", mult)
 
 

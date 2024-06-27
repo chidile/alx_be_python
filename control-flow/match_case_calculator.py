66fae577f3666528a75c0cc5d4997fc6c2faa2f1
Chimezie Chidile <chimex01.info@gmail.com>
2024-06-27 15:30:49 +0100

    Add Match case file

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+ops = input("Choose the operation (+, -, *, /): ")
+
+match ops:
+    case "+":
+        result = num1 + num2
+        print("The result is ", result)
+    case "-":
+        result = num1 - num2
+        print("The result is ", result)
+    case "*":
+        result = num1 * num2
+        print("The result is ", result)
+    case "/":
+        if num2 != 0:
+            result = num1 / num2
+            print("The result is ", result)
+        else:
+            print("Cannot divide by zeroi.")
